19444208abb651a95230d00f0c14b3c31821d8bb
Ademola Adebayo <scdk@yahoo.com>
2022-04-13 16:31:37 +0100

    JavaScript DOM update

--- mobile.js
+++ mobile.js
@@ -1,9 +1,35 @@
 // mobile JavaScript DOM
-const navSlide = () => {
+/*const navSlide = () => {
   const burger = document.querySelector('.burger');
   const navbar = document.querySelector('.nav-links');
   burger.addEventListener('click', ()=> {
     navbar.classList.toggle('nav-active');
   });
 }
-navSlide();
+navSlide();*/
+
+const burger = document.querySelector('.burger');
+const close = document.querySelector('.close');
+const menu = document.querySelector('.mobile')
+const links = document.querySelectorAll('.menu-links')
+
+burger.addEventListener('click', (event)=>{
+  if (event.target) {
+    menu.style.display = 'block'
+  }
+})
+
+close.addEventListener('click', (event)=>{
+  if (event.target) {
+    menu.style.display = 'none'
+  }
+})
+
+links.forEach((link)=>{
+  link.addEventListener('click', (event)=>{
+    if (event.target) {
+      menu.style.display = 'none'
+    }
+  })
+  
+})
